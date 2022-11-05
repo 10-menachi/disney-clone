5e27c0beb5161ee2d9ede817bafd8c46ee246b17
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-11-05 12:27:11 +0300

    added the nav section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,9 @@ function Header() {
   return (
     <nav className='flex justify-between items-center py-0 px-9 tracking-widest fixed top-0 left-0 right-0 h-20 bg-[#090B13] z-10'>
         <a href="" className='font-0 p-0 w-20 mt-1 max-h-20 inline-block'><img src="/images/logo.svg" alt="disney-logo" className='block w-full' /></a>
+        <div className='hidden md:flex items-center flex-row flex-nowrap h-full justify-end m-0 p-0 relative mr-auto'>
+            <h1>menu</h1>
+        </div>
         <a href="" className=''>Login</a>
     </nav>
   )
